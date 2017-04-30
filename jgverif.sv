96b4695549f2c1c4e9e5fd192941ee92adb7d2df
baraklidsky <baraklidsky@yahoo.com>
2017-04-30 14:17:56 -0500

    bugs

--- jgverif.sv
+++ jgverif.sv
@@ -1,21 +1,32 @@
 
+parameter
+  CBUS_CMD_WIDTH           = 3,
+  ADDR_WIDTH               = 32,
+  BROAD_TYPE_WIDTH         = 2,
+  BROAD_ID_WIDTH           = 5,  
+  BROAD_REQ_FIFO_SIZE      = 4,
+  BROAD_REQ_FIFO_SIZE_LOG2 = 2,
+  MBUS_CMD_WIDTH           = 3,
+  BREQ_FIFO_SIZE           = 2,
+  BREQ_FIFO_SIZE_LOG2      = 1;
+
 module v_mesi(
-input                   clk;          // System clock
-input                   rst;          // Active high system reset
+input                   clk,          // System clock
+input                   rst,          // Active high system reset
 // Main buses
-input [MBUS_CMD_WIDTH-1:0] mbus_cmd3_i; // Main bus3 command
-input [MBUS_CMD_WIDTH-1:0] mbus_cmd2_i; // Main bus2 command
-input [MBUS_CMD_WIDTH-1:0] mbus_cmd1_i; // Main bus1 command
-input [MBUS_CMD_WIDTH-1:0] mbus_cmd0_i; // Main bus0 command
+input [MBUS_CMD_WIDTH-1:0] mbus_cmd3_i, // Main bus3 command
+input [MBUS_CMD_WIDTH-1:0] mbus_cmd2_i, // Main bus2 command
+input [MBUS_CMD_WIDTH-1:0] mbus_cmd1_i, // Main bus1 command
+input [MBUS_CMD_WIDTH-1:0] mbus_cmd0_i, // Main bus0 command
 // Coherence buses
-input [ADDR_WIDTH-1:0]  mbus_addr3_i;  // Coherence bus3 address
-input [ADDR_WIDTH-1:0]  mbus_addr2_i;  // Coherence bus2 address
-input [ADDR_WIDTH-1:0]  mbus_addr1_i;  // Coherence bus1 address
-input [ADDR_WIDTH-1:0]  mbus_addr0_i;  // Coherence bus0 address
-input                   cbus_ack3_i;  // Coherence bus3 acknowledge
-input                   cbus_ack2_i;  // Coherence bus2 acknowledge
-input                   cbus_ack1_i;  // Coherence bus1 acknowledge
-input                   cbus_ack0_i;  // Coherence bus0 acknowledge
+input [ADDR_WIDTH-1:0]  mbus_addr3_i,  // Coherence bus3 address
+input [ADDR_WIDTH-1:0]  mbus_addr2_i,  // Coherence bus2 address
+input [ADDR_WIDTH-1:0]  mbus_addr1_i,  // Coherence bus1 address
+input [ADDR_WIDTH-1:0]  mbus_addr0_i,  // Coherence bus0 address
+input                   cbus_ack3_i,  // Coherence bus3 acknowledge
+input                   cbus_ack2_i,  // Coherence bus2 acknowledge
+input                   cbus_ack1_i,  // Coherence bus1 acknowledge
+input                   cbus_ack0_i  // Coherence bus0 acknowledge
 );
 
 endmodule
@@ -49,3 +60,4 @@ bind mesi_isc v_mesi mesi_props(
      .mbus_ack1_o      (mbus_ack_mesi_isc[1]),
      .mbus_ack0_o      (mbus_ack_mesi_isc[0])
      );
+endmodule
