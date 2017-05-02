e53cfa811d871ec1b1da6d7bfe7ef0ee78b06df8
baraklidsky <baraklidsky@yahoo.com>
2017-05-01 22:00:54 -0500

    tcl file

---
+++ lab4.tcl
@@ -0,0 +1,19 @@
+#Tcl script which can be used with JasperGold
+#Use "source lab4.tcl" in the console to source this script
+
+#Reading the files 
+analyze -verilog {mesi_isc_tb_cpu.v};
+analyze -sv {jgverif.sv};
+
+#Elaborating the design
+elaborate -top {mesi_isc_tb_cpu};
+
+#You will need to add commands below
+#Set the clock
+clock clk
+#Set Reset
+reset "rst"
+
+#Prove all
+prove -bg -all
+#
